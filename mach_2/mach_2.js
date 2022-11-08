0ac92f2ca5f06ea6792c673756d05ef391dbc403
timf34 <farrelti@tcd.ie>
2022-11-08 18:00:56 -0500

    fixed js for sliders

--- mach_2/mach_2.js
+++ mach_2/mach_2.js
@@ -1,7 +1,7 @@
 // TODO: will probably want to create a JS class/ object for the sliders here.
 
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
+let slider = document.getElementById("myRange1");
+let output = document.getElementById("demo1");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
