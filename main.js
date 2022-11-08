501977fe48e4b71ab4afcdc7f541016210e5a573
timf34 <farrelti@tcd.ie>
2022-11-08 11:29:19 -0500

    basic javascript functionality implemented (visible through the console)

---
+++ main.js
@@ -0,0 +1,11 @@
+// const button = document.getElementById("computation_button");
+
+function doComputation(){
+    num_1 = document.getElementById("num1").value;
+    console.log("Here is num1:");
+    console.log(num_1);
+
+    num_2 = document.getElementById("num2").value;
+    console.log("Here is num2");
+    console.log(num_2);
+}
